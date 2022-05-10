b4171dda5ca844703ba420e5b3aea75e81e68934
YangLyuJimmy <jimmylyuyang@outlook.com>
2022-05-10 16:08:53 -0400

    Adjust the img and input, add functions template to capture input value

--- src/App.js
+++ src/App.js
@@ -1,20 +1,43 @@
 import React from 'react';
 import { Input } from 'antd';
+import { SearchOutlined } from '@ant-design/icons';
 
 class App extends React.Component {
   state = {
-
+    inputValue: ''
   }
+
+  updateInputValue = (e) => {
+    this.setState({
+      inputValue: e.target.value,
+    })
+  }
+
+  searchCity = () => {
+    
+  }
+
+
   render = () => (
-    <div id='InputContainer'>
-      <img src="BIGSMART.jpg" alt='One Big Smart!' style={{padding: 30}}></img>
-      <Input placeholder="Search For Your Favorite City!" size='large'/>
+    <div>
+      <img 
+        src="BIGSMART.jpg" 
+        alt='One Big Smart!' 
+        style={{padding: 30, display: "block", marginLeft: "auto", marginRight: "auto", marginTop: "5%"}}
+      ></img>
+      <Input 
+        prefix={<SearchOutlined />}
+        placeholder="Search For Your Favorite City!" 
+        size='large'
+        maxLength={50}
+        style={{ width: "30%", left: "35%"}}
+        onChange={this.updateInputValue}
+        onPressEnter={this.searchCity}
+      />
     </div>
-
-
-    
-    
   )
+
+  
 }
 
 export default App;
