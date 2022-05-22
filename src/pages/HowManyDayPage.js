7ae28ff17e6b83a73f04073df1b7d813ac99b2b8
fongziyjun16 <fongziyjun16@hotmail.com>
2022-05-22 17:35:32 -0400

    update HowManyDayPage.js

--- src/pages/HowManyDayPage.js
+++ src/pages/HowManyDayPage.js
@@ -1,13 +1,55 @@
+import { Row, Col, Space, Form, InputNumber, Button } from "antd";
 import { Component } from "react";
+import { useNavigate } from "react-router-dom";
 
 class HowManyDayPage extends Component {
+
+    handleConfirm = (values) => {
+        const { days: currDays } = values;
+        localStorage.setItem('days', currDays);
+        this.props.navigate("/map");
+    }
+
     render() {
         return (
             <>
-                <h1>This is page for choosing how many days</h1>
+                <Row align="middle" justify="center" style={{ height: '100%' }}>
+                    <Col>
+                        <Space direction="vertical" size="middle" style={{ display: 'flex' }}>
+                            <h1>How many days do you want to travel?</h1>
+                            <Form 
+                                name="daysForm"
+                                onFinish={this.handleConfirm}
+                                initialValues={{
+                                    'days': 5
+                                }}>
+                                <Form.Item>
+                                    <Form.Item name="days" noStyle>
+                                        <InputNumber min={1} max={10} />
+                                    </Form.Item>
+                                    <span className="ant-form-text"> days</span>
+                                </Form.Item>
+                                <Form.Item>
+                                    <Button type="primary" htmlType="submit">
+                                        Confirm
+                                    </Button>
+                                </Form.Item>
+                            </Form>
+                        </Space>
+                    </Col>
+                </Row>
             </>
         )
     }
 }
 
-export default HowManyDayPage;
+// export default function (props) {
+//     const navigate = useNavigate();
+//     return <PlacesPage {...props} navigate={navigate} />
+// };
+
+
+export default function(props) {
+    const navigate = useNavigate();
+    return <HowManyDayPage {...props} navigate={navigate} />
+};
