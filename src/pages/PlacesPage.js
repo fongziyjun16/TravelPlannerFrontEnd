8e68f6edf390db9d3106009d8913174073413d7c
AustinLiuZR <91038185+AustinLiuZR@users.noreply.github.com>
2022-05-22 15:16:20 -0400

    Update PlacesPage.js

--- src/pages/PlacesPage.js
+++ src/pages/PlacesPage.js
@@ -4,153 +4,72 @@ import {CheckCircleOutlined} from "@ant-design/icons";
 import {useNavigate} from "react-router-dom";
 import { Modal, Card } from 'antd';
 
-
 const { Meta } = Card;
-
-
 
 class PlacesPage extends Component {
 
-    
     state = {
         isModalVisible : false,
-        setIsModalVisible : false,
+        numberOfCard: 8,
+        selectedCard: 0,
+        selectedCardDescription: ""
     };
+
+    componentDidMount() {
+        this.setState({
+            numberOfCard: this.getRandomInt(10, 21)
+        })
+        // console.log(this.state.numberOfCard);
+    }
 
     handleContinue = () => {
         this.props.navigate('/days');
     }
     
-    
-    showModal = () => {
-
+    handleCardOnClick = (e) => {
+        // console.log(e.currentTarget.getAttribute("flag") * 1);
+        const cardIndex = e.currentTarget.getAttribute("flag") * 1;
         this.setState({
-            setIsModalVisible : true,
+            isModalVisible: true,
+            selectedCard: cardIndex,
+            selectedCardDescription: 'Current Card is ' + cardIndex
         });
-        
-    } 
-        
-    handleOk = () => {
-        this.setState({
-            setIsModalVisible : false,
-      });
     }
-    
-    handleCancel = () => {
+
+    handleModalCancel = () => {
         this.setState({
-            setIsModalVisible : false,
+            isModalVisible: false
         });
+    }
+
+    getRandomInt = (min, max) => {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min) + min); 
+        //The maximum is exclusive and the minimum is inclusive
     }
 
     render() {
         return (
             <>
-                <h1>Check out some beautiful places in this lovely city!</h1>
-               
-                <Space align = "center" direction = "vertical" size = "large">
-                <Row span= {5}>
-                <Col span= {6}>
-                <Card
-                    onClick={this.showModal}
-                    hoverable
-                    style={{ width: 280 }}
-                    cover={<img alt="example" src="https://www.california-tour.com/blog/wp-content/uploads/2017/08/Fotolia_LA-downtown-XL.jpg" />}
-                >
-                    <Meta title="A" description="This is a beautiful city." />
-                    
-                </Card>
-                <Modal 
-                    title="A" 
-                    visible = {this.state.setIsModalVisible} 
-                    onOk={this.handleOk} 
-                    onCancel = {this.handleCancel} 
-                    closable={false}>
-                    <p></p>
-                </Modal>
-                </Col>
-                <Col span= {6}>
-                <Card
-                    hoverable
-                    style={{ width: 280 }}
-                    cover={<img alt="example" src="https://www.california-tour.com/blog/wp-content/uploads/2017/08/Fotolia_LA-downtown-XL.jpg" />}
-                >
-                    <Meta title="B" description="This is a beautiful city." />
-                </Card>
-                <Modal 
-                    title="B" 
-                    visible = {this.state.setIsModalVisible} 
-                    onOk={this.handleOk} 
-                    onCancel = {this.handleCancel} 
-                    closable={false}>
-                    <p></p>
-                </Modal>
-                </Col>
-                <Col span= {6}>
-                <Card
-                    hoverable
-                    style={{ width: 280 }}
-                    cover={<img alt="example" src="https://www.california-tour.com/blog/wp-content/uploads/2017/08/Fotolia_LA-downtown-XL.jpg" />}
-                >
-                    <Meta title="C" description="This is a beautiful city." />
-                
-                </Card>
-                </Col>
-                <Col span= {6}>
-                <Card
-                    hoverable
-                    style={{ width: 280 }}
-                    cover={<img alt="example" src="https://www.california-tour.com/blog/wp-content/uploads/2017/08/Fotolia_LA-downtown-XL.jpg" />}
-                >
-                    <Meta title="D" description="This is a beautiful city." />
-                
-                </Card>
-                
-                </Col>
-                <Col span= {6}>
-                <Card
-                    hoverable
-                    style={{ width: 280 }}
-                    cover={<img alt="example" src="https://www.california-tour.com/blog/wp-content/uploads/2017/08/Fotolia_LA-downtown-XL.jpg" />}
-                >
-                    <Meta title="E" description="This is a beautiful city." />
-                </Card>
-                
-                </Col>
-                <Col span= {6}>
-                <Card
-                    hoverable
-                    style={{ width: 280 }}
-                    cover={<img alt="example" src="https://www.california-tour.com/blog/wp-content/uploads/2017/08/Fotolia_LA-downtown-XL.jpg" />}
-                >
-                    <Meta title="F" description="This is a beautiful city." />
-                </Card>
-                
-                </Col>
-                <Col span= {6}>
-                <Card
-                    hoverable
-                    style={{ width: 280 }}
-                    cover={<img alt="example" src="https://www.california-tour.com/blog/wp-content/uploads/2017/08/Fotolia_LA-downtown-XL.jpg" />}
-                >
-                    <Meta title="G" description="This is a beautiful city." />
-                </Card>
-                
-                </Col>
-                <Col span= {6}>
-                <Card
-                    hoverable
-                    style={{ width: 280 }}
-                    cover={<img alt="example" src="https://www.california-tour.com/blog/wp-content/uploads/2017/08/Fotolia_LA-downtown-XL.jpg" />}
-                >
-                    <Meta title="H" description="This is a beautiful city." />
-                </Card>
-                
-                </Col>
-                </Row>
-                
-                </Space>
-                       
-                        <Form.Item wrapperCol={{ offset: 10, span: 9  }}>
+                <Space direction="vertical" size="middle" style={{ display: 'flex' }} >
+                    <h1>Check out some beautiful places in this lovely city!</h1>
+                    <Space size={[8, 16]} wrap>
+                        {new Array(this.state.numberOfCard).fill(null).map((_, index) => (
+                            <Card
+                                key={index} 
+                                flag={index}
+                                onClick={this.handleCardOnClick}
+                                title={"Photo " + index}
+                                hoverable
+                                style={{ width: 280 }}
+                                cover={<img alt="example" src="https://www.california-tour.com/blog/wp-content/uploads/2017/08/Fotolia_LA-downtown-XL.jpg" />}>
+                                <Meta title="G" description="This is a beautiful city." />
+                            </Card>                                
+                        ))}
+                    </Space>
+                    <Row align="middle" justify="center" gutter={[0, 16]}>
+                        <Col>
                             <Button
                                 typeof= "primary"
                                 shape= "round"
@@ -159,7 +78,17 @@ class PlacesPage extends Component {
                                 onClick={this.handleContinue}>
                                 Ready to build your Travel Plan?
                             </Button>
-                        </Form.Item>  
+                        </Col>
+                    </Row>
+                </Space>
+                <Modal  
+                    title={"Card " + this.state.selectedCard} 
+                    visible={this.state.isModalVisible}
+                    footer={null}
+                    onCancel={this.handleModalCancel}
+                    >
+                    <span>{"Description: "  + this.state.selectedCardDescription}</span>
+                </Modal>
             </>
         )
     }
