afe965e64423636298c993019ea4197522e4feae
fongziyjun16 <fongziyjun16@hotmail.com>
2022-05-27 19:26:31 -0400

    update

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,60 +1,54 @@
-import { Component } from "react";
+import React, { Component } from "react";
 import {Button, Col, Form, Image, Input, Row, Space} from "antd";
-import logo from '../assets/images/Logo2.png'
+import logo from '../assets/images/Logo.png'
 import {SearchOutlined} from "@ant-design/icons";
 import { useNavigate } from "react-router-dom";
 
-class SearchPage extends Component {
+function SearchPage() {
 
-    state = {
+    const navigate = useNavigate();
 
-    };
-
-    handleSearch = (values) => {
+    function handleSearch(values) {
         const { searchWord: keyWord } = values;
-        console.log("search word: " + keyWord);
-        this.props.navigate('/places');
+        // console.log("search word: " + keyWord);
+        localStorage.setItem('center', keyWord);
+        navigate('/places');
     }
 
-    render() {
-        return (
-            <>
-                <Row>
-                    <Col span={8}></Col>
-                    <Col span={8}>
-                        <Space direction="vertical" size="middle">
-                            <Image src={logo} preview={false}/>
-                            <Form
-                                name="searchForm"
-                                labelCol={{ span: 0 }}
-                                wrapperCol={{ span: 24 }}
-                                onFinish={this.handleSearch}>
-                                <Form.Item
-                                    name="searchWord"
-                                    rules={[{required: true, message: 'Please input your search word!'}]}>
-                                    <Input />
-                                </Form.Item>
-                                <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-                                    <Button
-                                        typeof="primary"
-                                        shape="round"
-                                        size="large"
-                                        icon={<SearchOutlined  />}
-                                        htmlType="submit">
-                                        Go
-                                    </Button>
-                                </Form.Item>
-                            </Form>
-                        </Space>
-                    </Col>
-                    <Col span={8}></Col>
-                </Row>
-            </>
-        )
-    }
+    return (
+        <>
+            <Row>
+                <Col span={8}></Col>
+                <Col span={8}>
+                    <Space direction="vertical" size="middle">
+                        <Image src={logo} preview={false}/>
+                        <Form
+                            name="searchForm"
+                            labelCol={{ span: 0 }}
+                            wrapperCol={{ span: 24 }}
+                            onFinish={handleSearch}>
+                            <Form.Item
+                                name="searchWord"
+                                rules={[{required: true, message: 'Please input your search word!'}]}>
+                                <Input />
+                            </Form.Item>
+                            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                                <Button
+                                    typeof="primary"
+                                    shape="round"
+                                    size="large"
+                                    icon={<SearchOutlined  />}
+                                    htmlType="submit">
+                                    Go
+                                </Button>
+                            </Form.Item>
+                        </Form>
+                    </Space>
+                </Col>
+                <Col span={8}></Col>
+            </Row>
+        </>
+    );
 }
 
-export default function (props) {
-    const navigate = useNavigate();
-    return <SearchPage {...props} navigate={navigate} />
-};
+export default SearchPage;
